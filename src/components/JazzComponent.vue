cfc922aa9b6786ece184b72a0dbdbf3e3c900390
Miki Blasco <blascom@7@gmail.com>
2023-01-08 15:18:25 +0100

    creation

---
+++ src/components/JazzComponent.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <img alt="Vue logo" src="../assets/logo.png" class="jazzPic" />
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "JazzComponent",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.jazzPic {
+  border-radius: 50%;
+  width: 200px;
+}
+</style>
---
+++ src/components/SecondComponent.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="secondComponent">
+    <button v-on:click="show()">Jazz</button>
+    <div v-if="showInfo" class="list">
+      <div v-for="item in arrayProp" :key="item">
+        {{ item }}
+      </div>
+      <a href="https://mikiblasco.github.io/Jazz-Wine-Canvas-Game/">
+        <button>Play!</button>
+      </a>
+    </div>
+  </div>
+</template>
+
+<script lang="js">
+
+  export default  {
+    name: 'SecondComponent',
+    props: [],
+    mounted () {
+    },
+    data () {
+      return {
+        arrayProp: ["The", "empty", "verse"],
+        showInfo: false
+      }
+    },
+    methods: {
+      show(){
+        if (this.showInfo === true) {
+          console.log("was true and now false", this.showInfo)
+         return this.showInfo = false
+        } else {
+          console.log("was false and now true", this.showInfo)
+          return this.showInfo = true
+        }
+      }
+    },
+    computed: {
+
+    }
+}
+</script>
+
+<style scoped>
+.list {
+  margin: 10px;
+}
+</style>
